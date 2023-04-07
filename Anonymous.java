9f21123e9094b75fa622f0e19c65827ec141aa97
Anshujha@031 <naveenjha344@gmail.com>
2023-04-07 20:58:56 +0530

    first commit

---
+++ Anonymous.java
@@ -0,0 +1,34 @@
+// 
+class Anonymous
+{
+    public static void main(String[]args)
+    {
+        int []a={1,2,3,4,5};
+
+        for(int n:a)
+        {
+            System.out.println(n);
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+// public class rough {
+//     public static void main(String[]args)
+//     {
+//         int []num={1,2,3,4};
+//         System.out.println(num.length);
+//     }
+    
+// }
